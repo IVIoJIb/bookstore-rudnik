4e1b3307129d1945a751fd9214f59769305954fb
IVIoJlb <iviojlb@MacBook-Air-IVIoJlb.local>
2026-03-07 20:59:54 +0200

    update main message in main branch

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from models import Book
 
+
 def create_book():
     title = input("Введіть назву книги: ")
     author = input("Введіть автора: ")
     price = float(input("Введіть ціну: "))
+
+    # Замість log_action додаємо простий print іншою мовою
+    print(f"[DEBUG]: Action performed - Created book object: '{title}'")
+
     return Book(title, author, price)
+
 
 if __name__ == "__main__":
     book = create_book()
