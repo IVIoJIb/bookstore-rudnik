cf103f5c6db07c7abd7784c0de3a4895e50d3d22
IVIoJlb <iviojlb@MacBook-Air-IVIoJlb.local>
2026-03-07 21:42:40 +0200

    add description field to Book model

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from models import Book
-
+from utils import validate_price
+from utils import log_action
 
 def create_book():
+    price = float(input("Введіть ціну: "))
     title = input("Введіть назву книги: ")
     author = input("Введіть автора: ")
     price = float(input("Введіть ціну: "))
-
-    # Замість log_action додаємо простий print іншою мовою
-    print(f"[DEBUG]: Action performed - Created book object: '{title}'")
-
+    validate_price(price)
+    log_action(f"Спроба створення книги: {title}")
     return Book(title, author, price)
-
 
 if __name__ == "__main__":
     book = create_book()
--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 # models.py
 class Book:
-    def __init__(self, title, author, price):
+    def __init__(self, title, author, price, description=""):
         self.title = title
         self.author = author
         self.price = price
-
+        self.description = description
     def __str__(self):
         return f"{self.title} від {self.author} - {self.price} грн"
