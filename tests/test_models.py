abcd2a1b06e7527b286aeaa71fcffc54ed6d7a6b
IVIoJlb <iviojlb@MacBook-Air-IVIoJlb.local>
2026-03-08 16:10:48 +0200

    commit that breaks CI test

---
+++ tests/test_models.py
@@ -0,0 +1,9 @@
+import unittest
+from models import Book
+
+class TestBookModel(unittest.TestCase):
+    def test_book_creation(self):
+        # Перевіряємо, чи правильно створюється об'єкт
+        book = Book("Kobzar", "Taras Shevchenko", 500)
+        self.assertEqual(book.title, "Kobzar")
+        self.assertEqual(book.price, 600)
